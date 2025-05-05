1cb2954bec72c9af35b7dfcd771eae6e9a106a96
fardanaljihad <fardanaljihad.18@gmail.com>
2025-05-05 20:41:27 +0700

    update: modify npc

--- npc/neg_prompt_calibration.py
+++ npc/neg_prompt_calibration.py
@@ -1,10 +1,28 @@
 import random
 import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.ndimage import label
+
 
 def calculate_iou(mask1, mask2):
     intersection = torch.logical_and(mask1, mask2)
     union = torch.logical_or(mask1, mask2)
     iou = torch.sum(intersection).float() / torch.sum(union).float()
+    return iou
+
+def calc_iou(prd, gt):
+    if isinstance(gt, torch.Tensor):
+        gt = gt.squeeze(0).cpu().numpy()
+        
+    assert prd.shape == gt.shape
+    prd = prd.reshape(prd.size).copy()
+    gt = gt.reshape(gt.size)
+
+    area_intersection = np.logical_and(prd, gt).sum()
+    area_union = np.logical_or(prd, gt).sum()
+
+    iou = area_intersection / (area_union + 1e-10)
     return iou
 
 def calc_iou_matrix(mask_list1, mask_list2):
@@ -33,44 +51,49 @@ def cal_mask_ious(
 
 
 def neg_prompt_calibration(
-    cfg,
-    mask_ious,
-    prompts,
+    masks,
 ):
-    '''
-    mask_ious:[mask_nums,mask_nums]
-    '''
-    point_list = []
-    point_labels_list = []
-    num_points = cfg.num_points
-    for m in range(len(mask_ious)):
-            
-        pos_point_coords = prompts[0][0][m][:num_points].unsqueeze(0) 
-        neg_point = prompts[0][0][m][num_points:].unsqueeze(0)  
-        neg_points_list = []
-        neg_points_list.extend(neg_point[0])
+    water_bodies = waterbodies_extraction(image_path)
 
-        indices = torch.nonzero(mask_ious[m] > float(cfg.iou_thr)).squeeze(1)
+    labeled_array, num_features = label(water_bodies)
+    individual_features = [(labeled_array == i).astype(np.uint8) for i in range(1, num_features + 1)]
+    
+    neg_points = []
+    for i in range(1, num_features + 1):
+        iou = calc_iou(masks[0], individual_features[i-1])
+        neg_coords = np.argwhere(individual_features[i-1] > 0)\
+        
+        gt = masks[0]
+        pred = individual_features[i-1]
+        
+        gt_bool = gt.astype(bool)
+        pred_bool = pred.astype(bool)
+        
+        overlay = np.zeros((*gt.shape, 3), dtype=np.uint8)
+        overlay[gt_bool] = [255, 195, 128]
+        overlay[pred_bool] = [0, 0, 255]
+        overlay[gt_bool & pred_bool] = [255, 255, 0]
+        
+        fig, axes = plt.subplots(1, 3, figsize=(15, 5))
+        
+        axes[0].imshow(gt, cmap='gray')
+        axes[0].set_title('Init Result')
+        axes[0].axis('off')
+        
+        axes[1].imshow(pred, cmap='gray')
+        axes[1].set_title(f'Water {i}')
+        axes[1].axis('off')
+        
+        axes[2].imshow(overlay)
+        axes[2].set_title('Intersection')
+        axes[2].axis('off')
+        
+        plt.tight_layout()
+        plt.show()
 
-        if indices.numel() != 0:
-            # neg_points_list = []
-            for indice in indices:
-                neg_points_list.extend(prompts[0][0][indice][:num_points])
-            neg_points = random.sample(neg_points_list, num_points)
-        else:
-            neg_points =neg_points_list
-            
-        neg_point_coords = torch.tensor([p.tolist() for p in neg_points], device=neg_point.device).unsqueeze(0)
+        if iou > 0.1:
+            neg_random_point = neg_coords[np.random.randint(len(neg_coords))]
+            neg_points.append([neg_random_point[1], neg_random_point[0]])
+            neg_labels = np.zeros(len(neg_points), dtype=int)
 
-        point_coords = torch.cat((pos_point_coords, neg_point_coords), dim=1) 
-
-        point_list.append(point_coords)
-        pos_point_labels = torch.ones(pos_point_coords.shape[0:2], dtype=torch.int, device=neg_point.device)
-        neg_point_labels = torch.zeros(neg_point_coords.shape[0:2], dtype=torch.int, device=neg_point.device)
-        point_labels = torch.cat((pos_point_labels, neg_point_labels), dim=1)  
-        point_labels_list.append(point_labels)
-
-    point_ = torch.cat(point_list).squeeze(1)
-    point_labels_ = torch.cat(point_labels_list)
-    new_prompts = [(point_, point_labels_)]
-    return new_prompts
+    return neg_points, neg_labels
